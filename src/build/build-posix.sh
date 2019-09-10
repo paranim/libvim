07eeea21e8f13cc675144aa33c40ce78d9317120
oakes <zsoakes@gmail.com>
2019-09-10 12:56:00 -0400

    Don't link to libobjc

--- src/build/build-posix.sh
+++ src/build/build-posix.sh
@@ -2,7 +2,7 @@ unameOut="$(uname -s)"
 
 case "${unameOut}" in
     Linux*) CFLAGS="CFLAGS=-fPIC" LIBNAME="libvim.so" LIBFLAGS="-lncurses";;
-    *)      CFLAGS="" LIBNAME="libvim.dylib" LIBFLAGS="-liconv -lncurses -lobjc -framework CoreFoundation -framework AppKit";;
+    *)      CFLAGS="" LIBNAME="libvim.dylib" LIBFLAGS="-liconv -lncurses -framework CoreFoundation -framework AppKit";;
 esac
 
 ./configure --disable-selinux ${CFLAGS}
